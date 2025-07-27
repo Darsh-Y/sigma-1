719a41c810a2bc98e76162f2a8c4560b46285f92
Darsh Y <darshanyavagal@example.com>
2025-07-27 19:54:34 +0530

    fixing error1

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ app.use((err, req, res, next) => {
   // res.status(statusCode).send(message);
 });
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`app is listening at the port:${port}`);
 });
